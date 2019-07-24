be5f593d83e7f17917c603432c60ced4606e838b
Filip van Laenen <f.a.vanlaenen@ieee.org>
2019-07-24 15:37:03 +0200

    Removed some Checkstyle violations in HemicycleLayoutExporterTest

--- src/test/java/net/filipvanlaenen/shecc/export/HemicycleLayoutExporterTest.java
+++ src/test/java/net/filipvanlaenen/shecc/export/HemicycleLayoutExporterTest.java
@@ -35,11 +35,13 @@ public class HemicycleLayoutExporterTest {
         HemicycleLayout layout = new HemicycleLayout(THREE);
         HemicycleLayoutExporter exporter = new HemicycleLayoutExporter();
         String actual = exporter.export(layout);
-        String expected = "<svg height=\"1100\" viewBox=\"-1.05 -1.05 2.1 1.1\" width=\"2100\" xmlns=\"http://www.w3.org/2000/svg\">\n"
+        String expected = "<svg height=\"1100\" viewBox=\"-1.05 -1.05 2.1 1.1\" width=\"2100\""
+                + " xmlns=\"http://www.w3.org/2000/svg\">\n"
                 + "  <circle cx=\"-0.57735\" cy=\"-0.333333\" fill=\"#777777\" r=\"0.3\"/>\n"
                 + "  <circle cx=\"0\" cy=\"-0.666667\" fill=\"#777777\" r=\"0.3\"/>\n"
                 + "  <circle cx=\"0.57735\" cy=\"-0.333333\" fill=\"#777777\" r=\"0.3\"/>\n"
-                + "  <text fill=\"#000000\" font-size=\"0.021\" text-anchor=\"end\" transform=\"rotate(270 1.05,-1.05)\" x=\"1.0395\" y=\"-1.0605\">Chart produced using SHecC</text>\n"
+                + "  <text fill=\"#000000\" font-size=\"0.021\" text-anchor=\"end\""
+                + " transform=\"rotate(270 1.05,-1.05)\" x=\"1.0395\" y=\"-1.0605\">Chart produced using SHecC</text>\n"
                 + "</svg>";
         assertEquals(expected, actual);
     }
@@ -53,12 +55,14 @@ public class HemicycleLayoutExporterTest {
         HemicycleLayout layout = new HemicycleLayout(THREE, JUST_BELOW_PI);
         HemicycleLayoutExporter exporter = new HemicycleLayoutExporter();
         String actual = exporter.export(layout);
-        String expected = "<svg height=\"1100\" viewBox=\"-1.049983 -1.05 2.099966 1.1\" width=\"2099.966403\" xmlns=\"http://www.w3.org/2000/svg\">\n"
+        String expected = "<svg height=\"1100\" viewBox=\"-1.049983 -1.05 2.099966 1.1\" width=\"2099.966403\""
+                + " xmlns=\"http://www.w3.org/2000/svg\">\n"
                 + "  <circle cx=\"-0.576058\" cy=\"-0.335562\" fill=\"#777777\" r=\"0.3\"/>\n"
                 + "  <circle cx=\"0\" cy=\"-0.666667\" fill=\"#777777\" r=\"0.3\"/>\n"
                 + "  <circle cx=\"0.576058\" cy=\"-0.335562\" fill=\"#777777\" r=\"0.3\"/>\n"
-                + "  <text fill=\"#000000\" font-size=\"0.021\" text-anchor=\"end\" transform=\"rotate(270 1.049983,-1.05)\" x=\"1.039483\" y=\"-1.0605\">Chart produced using SHecC</text>\n"
-                + "</svg>";
+                + "  <text fill=\"#000000\" font-size=\"0.021\" text-anchor=\"end\""
+                + " transform=\"rotate(270 1.049983,-1.05)\" x=\"1.039483\" y=\"-1.0605\">Chart produced using"
+                + " SHecC</text>\n" + "</svg>";
         assertEquals(expected, actual);
     }
 
@@ -71,11 +75,13 @@ public class HemicycleLayoutExporterTest {
         HemicycleLayout layout = new HemicycleLayout(THREE, JUST_ABOVE_PI);
         HemicycleLayoutExporter exporter = new HemicycleLayoutExporter();
         String actual = exporter.export(layout);
-        String expected = "<svg height=\"1104.203661\" viewBox=\"-1.05 -1.05 2.1 1.104204\" width=\"2100\" xmlns=\"http://www.w3.org/2000/svg\">\n"
+        String expected = "<svg height=\"1104.203661\" viewBox=\"-1.05 -1.05 2.1 1.104204\" width=\"2100\""
+                + " xmlns=\"http://www.w3.org/2000/svg\">\n"
                 + "  <circle cx=\"-0.578282\" cy=\"-0.331714\" fill=\"#777777\" r=\"0.3\"/>\n"
                 + "  <circle cx=\"0\" cy=\"-0.666667\" fill=\"#777777\" r=\"0.3\"/>\n"
                 + "  <circle cx=\"0.578282\" cy=\"-0.331714\" fill=\"#777777\" r=\"0.3\"/>\n"
-                + "  <text fill=\"#000000\" font-size=\"0.021\" text-anchor=\"end\" transform=\"rotate(270 1.05,-1.05)\" x=\"1.0395\" y=\"-1.0605\">Chart produced using SHecC</text>\n"
+                + "  <text fill=\"#000000\" font-size=\"0.021\" text-anchor=\"end\""
+                + " transform=\"rotate(270 1.05,-1.05)\" x=\"1.0395\" y=\"-1.0605\">Chart produced using SHecC</text>\n"
                 + "</svg>";
         assertEquals(expected, actual);
     }

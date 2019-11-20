2a2cf141cb2b82ec4da23c1e1befba97744ffee3
Filip van Laenen <f.a.vanlaenen@ieee.org>
2019-11-20 08:22:05 +0100

    Resolved some Checkstyle violations

--- src/main/java/net/filipvanlaenen/shecc/SeatStatus.java
+++ src/main/java/net/filipvanlaenen/shecc/SeatStatus.java
@@ -1,6 +1,19 @@
 package net.filipvanlaenen.shecc;
 
+/**
+ * An enumeration holding the possible statuses for seats.
+ */
 public enum SeatStatus {
-    CERTAIN, LIKELY, UNCERTAIN
-
+    /**
+     * A certain seat, i.e. below the lower bound of the confidence interval.
+     */
+    CERTAIN,
+    /**
+     * A likely seat, i.e. in the lower half of the confidence interval.
+     */
+    LIKELY,
+    /**
+     * An uncertain seat, i.e. in the upper half of the confidence interval.
+     */
+    UNCERTAIN
 }

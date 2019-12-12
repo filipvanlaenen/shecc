991472b6dbd5b7dc996915fefa557b74340139ed
Filip van Laenen <f.a.vanlaenen@ieee.org>
2019-12-12 12:34:08 +0100

    Removed opacity from SeatStatus, as it isn't in use any more

--- src/main/java/net/filipvanlaenen/shecc/SeatStatus.java
+++ src/main/java/net/filipvanlaenen/shecc/SeatStatus.java
@@ -7,37 +7,13 @@ public enum SeatStatus {
     /**
      * A certain seat, i.e. below the lower bound of the confidence interval.
      */
-    CERTAIN(1.0D),
+    CERTAIN(),
     /**
      * A likely seat, i.e. in the lower half of the confidence interval.
      */
-    LIKELY(0.5D),
+    LIKELY(),
     /**
      * An unlikely seat, i.e. in the upper half of the confidence interval.
      */
-    UNLIKELY(0.2D);
-
-    /**
-     * The opacity for the seat status.
-     */
-    private final double opacity;
-
-    /**
-     * Constructs a seat status with an opacity.
-     *
-     * @param opacity
-     *            The opacity for the seat status.
-     */
-    SeatStatus(final double opacity) {
-        this.opacity = opacity;
-    }
-
-    /**
-     * Returns the opacity for the seat status.
-     *
-     * @return The opacity for the seat status.
-     */
-    public double getOpacity() {
-        return opacity;
-    }
+    UNLIKELY();
 }
